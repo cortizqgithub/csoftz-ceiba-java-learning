c82e4c32cd3c6f044b2af9c33b665767ec7274f4
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-09-23 18:59:13 -0500

    Adds first commit with the initial project structure.

---
+++ app-code/server/parking-service/src/main/java/com/csoftz/ceiba/java/learn/parking/service/ParkingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.csoftz.ceiba.java.learn.parking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingServiceApplication.class, args);
+	}
+}
