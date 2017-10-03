07638ce61772db32c9e08bea256c807fd0441649
Carlos Adolfo Ortiz Q (Ceiba) <carlosaortiz@yahoo.com>
2017-10-03 11:17:11 -0500

    Adds libraries for parking-domain and parking-commons as libraries and updates docs.

---
+++ app-code/server/build-artifacts.bat
@@ -0,0 +1,12 @@
+cd parking-domain
+gradlew clean build install
+cd ..
+cd parking-commons
+gradlew clean build install
+cd ..
+cd parking-service
+gradlew clean build
+cd ..
+cd parking-web
+gradlew clean build
+cd ..
