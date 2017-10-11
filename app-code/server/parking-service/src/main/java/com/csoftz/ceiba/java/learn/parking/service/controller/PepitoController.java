a69c62f80bfb6d573ebaee26863310e2cb07a058
Carlos Adolfo Ortiz Q (Ceiba) <carlosaortiz@yahoo.com>
2017-10-11 13:09:53 -0500

    Removes file as it is not longer needed.

--- app-code/server/parking-service/src/main/java/com/csoftz/ceiba/java/learn/parking/service/controller/PepitoController.java
+++
@@ -1,8 +0,0 @@
-package com.csoftz.ceiba.java.learn.parking.service.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PepitoController {
-
-}
