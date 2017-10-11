a8c3314f89bb541b3434b50680d858605f02c1b6
Carlos Adolfo Ortiz Q (Ceiba) <carlosaortiz@yahoo.com>
2017-10-11 11:38:12 -0500

    Adds new Controller

---
+++ app-code/server/parking-service/src/main/java/com/csoftz/ceiba/java/learn/parking/service/controller/PepitoController.java
@@ -0,0 +1,8 @@
+package com.csoftz.ceiba.java.learn.parking.service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PepitoController {
+
+}
